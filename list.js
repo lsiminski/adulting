786b70e28c95848b2472c9720238e064339c1226
lsiminski <lsiminski@mica.edu>
2015-10-22 12:46:33 -0400

    asdfasdf

--- list.js
+++ list.js
@@ -2,7 +2,7 @@ $.getJSON('list.json', function(data) {
     
   var randomItem = data[Math.floor(Math.random()*data.length)];
   console.log(randomItem.name);
-  $("#list_name p").html("<a href='" + randomItem.url + "'>" + randomItem.name + "</a>");
+  $("#list_name p").html("<a href='" + randomItem.url + "' target=blank>" + randomItem.name + "</a>");
   $("#list_day p").text(randomItem.day);
   $("#list_category p").text(randomItem.category);
   $("body.random").css("background-image","url(" + randomItem.image + ")");
