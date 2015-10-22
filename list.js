5ddd523a874043f3daa76868441111868ae2cb63
lsiminski <lsiminski@mica.edu>
2015-10-22 10:54:52 -0400

    asdf

--- list.js
+++ list.js
@@ -3,5 +3,7 @@ $.getJSON('list.json', function(data) {
   var randomItem = data[Math.floor(Math.random()*data.length)];
   console.log(randomItem.name);
   $("#list_name p").text(randomItem.name);
+  $("#list_date p").text(randomItem.date);
+  $("#list_category p").text(randomItem.category);
     
 });
