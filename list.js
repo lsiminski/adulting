4d7aa85c2ab22167c792600a58ec56e81d454eac
lsiminski <lsiminski@mica.edu>
2015-10-22 14:11:55 -0400

    asdf

--- list.js
+++ list.js
@@ -1,11 +1,19 @@
 $.getJSON('list.json', function(data) { 
+
+  var d = new Date();
+  var mo = d getMonth();
+  var da = d getDate ();
+  var moda = mo + 1 +"-" + da;
     
-  var randomItem = data[Math.floor(Math.random()*data.length)];
-  console.log(randomItem.name);
-  $("#list_name p").html("<a href='" + randomItem.url + "' target=blank>" + randomItem.name + "</a>");
-  $("#list_day p").text(randomItem.day);
-  $("#list_category p").text(randomItem.category);
-  $("body.random").css("background-image","url(" + randomItem.image + ")");
+  $.each(data, function(data) {
+	  if ( this.date === moda ) {
+		  $("#list_name p").html("<a href='" + this.url + "' target=blank>" + this.name + "</a>");
+		  $("#list_day p").text(this.day);
+		  $("#list_category p").text(this.category);
+		  $("body.random").css("background-image","url(" + this.image + ")");
+	  } else {
+	  }
+  });
     
 });
 
